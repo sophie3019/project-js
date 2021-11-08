6fc2061999eb64daa78dce1093b4de021dbb2e4e
unknown <sophie.sa43@gmail.com>
2021-11-08 15:39:23 +0200

    added display

--- style.js
+++ style.js
@@ -1,1 +1,8 @@
-const persons = ["Sophie","Lucky","Wandiloe","Andisiwe","Mduzudi","Kagisho","Happy",];
+const persons = ["Sophie","Lucky","Wandiloe","Andisiwe","Mduzudi","Kagisho","Happy","Sithembiso","Precious","Seline","Nkosi","Ntombela","Vuyo","Khum","Lebo"];
+
+const display = document.getElementById("display");
+
+for (let i = 0; i < persons.length; i++) {
+    display
+    
+}
